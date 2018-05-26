a1906e15541873333adafa3d52ac13bb6dfd75e5
ilss <375259904@qq.com>
2018-05-26 23:49:48 +0800

    首页部分代码

---
+++ src/components/btn.vue
@@ -0,0 +1,28 @@
+<template>
+    <span :class="[btnClass,cname]">
+        <slot/>
+    </span>
+</template>
+
+<script>
+export default {
+    props: {
+        cname: {
+            type: String,
+            default: "",
+        },
+    },
+    data () {
+        return {
+            btnClass: "btn",
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+@import "../css/element.scss";
+.btn {
+  @include btn;
+}
+</style>
---
+++ src/home/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Heador/>
+        <Slider/>
+        <Novice/>
+    </div>
+</template>
+
+<script>
+import Heador from '../public/header'
+import Slider from './hslider'
+import Novice from './novice'
+export default {
+    components: {
+        Heador,
+        Slider,
+        Novice,
+    },
+}
+</script>
+
+<style lang='scss' scoped>
+@import "../css/reset.scss";
+</style>
