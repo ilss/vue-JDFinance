091f74aee919dcd9a3595d7d3b52549f9b5d9677
ilss <375259904@qq.com>
2018-05-27 20:57:14 +0800

    众筹页面

---
+++ src/raise/index.vue
@@ -0,0 +1,35 @@
+ <template>
+    <div>
+        <Heador/>
+        <Slider/>
+        <Daily/>
+        <In/>
+        <More/>
+        <Footer />
+        <Navbar/>
+    </div>
+</template>
+
+<script>
+import Heador from '../public/header'
+import Slider from './rslider'
+import Footer from '../public/footer'
+import Navbar from '../public/navbar'
+import Daily from './daily'
+import In from './in'
+import More from './more'
+export default {
+    components: {
+        Heador,
+        Slider,
+        Footer,
+        Navbar,
+        Daily,
+        In,
+        More,
+    },
+}
+</script>
+
+<style lang='scss'>
+</style>
